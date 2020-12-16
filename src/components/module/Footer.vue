e6dae2fec4e330bcf3b87f2b7ba65e67ba82def5
eggyatma2908 <eggyatmariansyah@gmail.com>
2020-12-16 17:55:33 +0700

    Component Header & Footer

---
+++ src/components/module/Footer.vue
@@ -0,0 +1,171 @@
+<template>
+    <footer>
+        <div class="d-flex justify-content-between">
+            <div class="box5">
+                <div class="box6">
+                    <img class="img3" src="../../assets/coffee.png" alt="Image5">
+                    <p class="text7">Coffe Shop</p>
+                </div>
+                <p class="text8">Coffee Shop is a store that sells some good meals, and especially coffee. We provide high quality beans</p>
+                <a href="#"><img class="facebook" src="../../assets/facebook.png" alt="image6"></a>
+                <a href="#"><img class="twitter" src="../../assets/twitter.png" alt="image7"></a>
+                <a href="#"><img class="instagram" src="../../assets/instagram.png" alt="image8"></a>
+                <p class="text9">@2020CoffeStore</p>
+            </div>
+            <div class="box7">
+                <div class="product">
+                    <p>Product</p>
+                    <a href="#"><li>Download</li></a>
+                    <a href="#"><li>Pricing</li></a>
+                    <a href="#"><li>Locations</li></a>
+                    <a href="#"><li>Countries</li></a>
+                    <a href="#"><li>Blog</li></a>
+                </div>
+                <div class="engage">
+                    <p>Engage</p>
+                    <a href="#"><li>Coffe Shop ?</li></a>
+                    <a href="#"><li>FAQ</li></a>
+                    <a href="#"><li>About Us</li></a>
+                    <a href="#"><li>Privacy Policy</li></a>
+                    <a href="#"><li>Terms of Service</li></a>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped>
+footer {
+    padding-top: 181px;
+    background: #F8F8F8;
+}
+
+.box5 {
+    margin-left: 150px;
+}
+
+.box6 {
+    display: flex;
+}
+
+.img3 {
+    width: 30px;
+    height: 33px;
+}
+
+.box7 {
+    display: flex;
+    margin-right: 150px;
+}
+
+.text7 {
+    width: max-content;
+    height: max-content;
+
+    margin-top: 5px;
+    margin-left: 15px;
+
+    font-family: Rubik;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 20px;
+    line-height: 24px;
+
+    color: #0B132A;
+}
+
+.text8 {
+    width: 340px;
+    height: 61.57px;
+
+    margin-top: 22px;
+    margin-bottom: 45px;
+
+    font-family: Rubik;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 16px;
+    line-height: 30px;
+
+    color: #4F5665;
+}
+
+.facebook {
+    margin-right: 20px
+}
+
+.twitter {
+    margin-right: 20px;
+}
+.text9 {
+    margin-top: 30px;
+
+    font-family: Rubik;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 30px;
+
+    color: #AFB5C0;
+}
+
+.product {
+    margin-right: 126px;
+}
+
+.product p {
+    width: 71px;
+    height: 30px;
+
+    font-family: Rubik;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 30px;
+
+    color: #0B132A;
+}
+
+.product li {
+    list-style: none;
+
+    font-family: Rubik;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 30px;
+
+    color: #4F5665;
+}
+
+.engage p {
+    width: 66px;
+    height: 30px;
+
+    font-family: Rubik;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 18px;
+    line-height: 30px;
+
+    color: #0B132A;
+}
+
+.engage li {
+    list-style: none;
+
+    font-family: Rubik;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    line-height: 30px;
+
+    color: #4F5665;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <Header />
+    <Footer />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '@/components/module/Header.vue'
+import Footer from '@/components/module/Footer.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Header,
+    Footer
   }
 }
 </script>
