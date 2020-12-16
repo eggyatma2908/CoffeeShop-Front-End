25f90a02ade7efb4cebc985f67da229191fe7da1
Gefyaqiilah <gefyaqiilah26@gmail.com>
2020-12-17 00:07:43 +0700

    fix conflict

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,11 @@
 <template>
   <div>
-<<<<<<< HEAD
-=======
     <Header />
     <Footer />
->>>>>>> e6dae2fec4e330bcf3b87f2b7ba65e67ba82def5
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
-=======
 // @ is an alias to /src
 import Header from '@/components/module/Header.vue'
 import Footer from '@/components/module/Footer.vue'
@@ -22,5 +17,4 @@ export default {
     Footer
   }
 }
->>>>>>> e6dae2fec4e330bcf3b87f2b7ba65e67ba82def5
 </script>
