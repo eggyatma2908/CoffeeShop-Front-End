c9227e44648d3f9cb05e937ad0e34e37a8d30c88
eggyatma2908 <eggyatmariansyah@gmail.com>
2020-12-17 04:17:57 +0700

    Fisrt Commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Productdetails />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Productdetails from '@/components/module/Productdetails.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Productdetails
   }
 }
 </script>
