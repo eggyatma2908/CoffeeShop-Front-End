c3ace323864d71083eb4064095b5057ed0573728
sjasminetya <shaulajasminetya22@gmail.com>
2020-12-17 14:38:22 +0700

    fix conflict

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
   </div>
 </template>
 
-<<<<<<< HEAD
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap");
 @import url("https://fonts.googleapis.com/css2?family=Rubik:wght@700&display=swap");
