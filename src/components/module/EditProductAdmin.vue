e9a9a12270ddeccb69b9d54689a97b3b7ebf41d0
eggyatma2908 <eggyatmariansyah@gmail.com>
2020-12-18 18:44:47 +0700

    Third Commit

--- src/components/module/EditProductAdmin.vue
+++ src/components/module/EditProductAdmin.vue
@@ -20,28 +20,34 @@
             <div class="col-lg-6">
                 <div class="box4">
                     <div class="box5">
-                        <input class="productname" type="text" placeholder="Enter product name" v-model="productname">
-                        <input class="price" type="text" placeholder="Enter price" v-model="price">
-                        <input class="productdetail" type="text" placeholder="Enter details" v-model="productdetail">
+                        <input class="productname" type="text" placeholder="Enter product name" value="COLD BREW">
+                        <input class="price" type="text" placeholder="Enter price" value="IDR 30.000">
+                        <textarea class="productdetail" type="text" placeholder="Enter details" value="">Cold brewing is a method of brewing that combines ground coffee and cool water and uses time instead of heat to extract the flavor. It is brewed in small batches and steeped for as long as 48 hours.</textarea>
                     </div>
                 </div>
                 <div class="box6">
                     <select>
-                        <option >Select Size</option>
-                        <option >Audi</option>
-                        <option >BMW</option>
-                        <option >Citroen</option>
-                        <option >Ford</option>
-                        <option >Honda</option>
-                        <option >Jaguar</option>
-                        <option >Land Rover</option>
-                        <option >Mercedes</option>
-                        <option >Mini</option>
-                        <option >Nissan</option>
-                        <option >Toyota</option>
-                        <option >Volvo</option>
+                        <option disabled selected>Select Size</option>
+                        <option>R</option>
+                        <option>L</option>
+                        <option>XL</option>
+                    </select>
+                    <select>
+                        <option disabled selected>Select Delivery Methods</option>
+                        <option>Dine in</option>
+                        <option>Door delivery</option>
+                        <option>Pick up</option>
                     </select>
                 </div>
+                <div class="box7">
+                    <div class="box8">
+                        <button class="plus" type="submit" @click="plusCount">+</button>
+                        <p class="text4">{{count}}</p>
+                        <button class="minus" type="submit" @click="minusCount">-</button>
+                    </div>
+                    <button class="addcart" type="submit">Add to Cart</button>
+                </div>
+                <button class="editproduct" type="submit">Edit Product</button>
             </div>
         </div>
     </div>
@@ -52,9 +58,17 @@ export default {
   name: 'EditProductAdmin',
   data () {
     return {
-      productname: 'COLD BREW',
-      price: 'IDR 30.000',
-      productdetail: 'Cold brewing is a method of brewing that combines ground coffee and cool water and uses time instead of heat to extract the flavor. It is brewed in small batches and steeped for as long as 48 hours.'
+      count: 1
+    }
+  },
+  methods: {
+    plusCount () {
+      this.count = this.count + 1
+    },
+    minusCount () {
+      if (this.count > 0) {
+        this.count = this.count - 1
+      }
     }
   }
 }
@@ -138,6 +152,7 @@ export default {
     height: 70px;
 
     margin-left: -90px;
+    margin-bottom: 50px;
 
     font-family: Poppins;
     font-style: normal;
@@ -150,6 +165,8 @@ export default {
 .box4 {
     display: flex;
     align-items: center;
+
+    margin-top: 123px;
 }
 
 .box5 {
@@ -157,17 +174,63 @@ export default {
     flex-direction: column;
 }
 
+input::placeholder {
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 25px;
+
+    letter-spacing: 0.03em;
+}
+
+input:focus {
+    outline: none;
+}
+
 .productname {
+    width: 549px;
+
+    margin-bottom: 15px;
+
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: 900;
+    font-size: 65px;
+
     border: none;
     border-bottom: 1px solid #9F9F9F;
+
+    color: #000000;
 }
 
 .price {
+    margin-bottom: 15px;
+
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: 500;
+    font-size: 40px;
+    line-height: 60px;
+
     border: none;
     border-bottom: 1px solid #9F9F9F;
+
+    color: #000000;
 }
 
 .productdetail {
+    width: 549px;
+    height: 200px;
+
+    padding-bottom: 0px;
+    margin-bottom: 50px;
+
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 25px;
+
+    color: #000000;
     border: none;
     border-bottom: 1px solid #9F9F9F;
 }
@@ -177,6 +240,7 @@ select {
     height: 83px;
 
     padding-left: 30px;
+    margin-bottom: 30px;
 
     border: 1px solid #9F9F9F;
     box-sizing: border-box;
@@ -195,22 +259,111 @@ select {
     background: url('../../assets/arrow.png') 96% / 10% no-repeat #FFFFFF;
 }
 
-.selectdelivery {
-    width: 549px;
-    height: 83px;
+.box7 {
+    display: flex;
+    justify-content: space-between;
 
-    padding-left: 30px;
+    width: 550px;
+
+    margin-bottom: 30px;
+}
+
+.box8 {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+
+    width: 167px;
+    height: 83px;
 
     background: #FFFFFF;
     border: 1px solid #9F9F9F;
     box-sizing: border-box;
     border-radius: 10px;
+}
+
+.plus {
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: 900;
+    font-size: 24px;
+
+    color: #9F9F9F;
+
+    border: none;
+    background-color: transparent;
+}
+
+.plus:focus {
+    outline: none;
+}
+
+.text4 {
+    margin-bottom: 0px;
 
     font-family: Poppins;
     font-style: normal;
-    font-weight: normal;
-    font-size: 20px;
+    font-weight: 900;
+    font-size: 24px;
+
+    color: #000000;
+}
+
+.minus {
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: 900;
+    font-size: 24px;
 
     color: #9F9F9F;
+
+    border: none;
+    background-color: transparent;
 }
+
+.minus:focus {
+    outline: none;
+}
+
+.addcart {
+    width: 358px;
+    height: 83px;
+
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 25px;
+
+    color: #6A4029;
+
+    background: #FFBA33;
+    box-shadow: 0px 6px 20px rgba(255, 186, 51, 0.29);
+    border: none;
+    border-radius: 10px;
+}
+
+.addcart:focus {
+    outline: none;
+}
+
+.editproduct {
+    width: 549px;
+    height: 85px;
+
+    font-family: Poppins;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 25px;
+
+    color: #FFFFFF;
+
+    background: #6A4029;
+    border: none;
+    border-radius: 10px;
+}
+
+.editproduct:focus {
+    outline: none;
+}
+
 </style>
